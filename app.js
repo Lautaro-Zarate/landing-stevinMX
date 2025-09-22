e7c781b7f087010292e01ffadc02f9ead6234331
Lautaro Zarate <zlautaro43@gmail.com>
2025-09-22 20:28:26 -0300

    fix: fix some changes in translations and flags

--- app.js
+++ app.js
@@ -296,6 +296,7 @@ const translations = {
         machinesSubtitle: "Contamos con un moderno y equipado taller para así ofrecer resultados eficientes y producir con los más altos niveles de calidad.",
         
         // Machine Specifications
+        technologieTitle: "Tecnología",
         machineTitle: "Máquinas CNC",
         machineTitle2: "Máquinas de Inyección",
         machineTitle3: "Calidad",
@@ -340,8 +341,50 @@ const translations = {
         footerContact: "Contacto",
         footerCopyright: "© 2025 STEVIN. Todos los derechos reservados.",
         footerMadeBy: "Hecho por",
+        footerPrivacy: "Aviso de Privacidad",
         privacyPolicy: "Política de Privacidad",
-        termsOfUse: "Términos de Uso"
+        termsOfUse: "Términos de Uso",
+
+        // Aviso de privacidad
+        adviseTitle: "Identidad y domicilio del responsable",
+        adviseText: "Con fundamento en los artículos 15 y 16 de la Ley Federal de Protección de Datos Personales en Posesión de Particulares, STEVIN, Sociedad Anónima de Capital Variable (en adelante también referida como la “Sociedad”), por conducto de la Lic. Ana Luisa G. de Mutz, con domicilio en Calle 3RA Sur núm. 30, Colonia la Independencia, 54914, Tultitlán, Estado de México, ES RESPONSABLE del uso y protección de los datos personales que Usted como Titular proporcionará a la Sociedad.",
+
+        adviseTitle2: "Finalidades del tratamiento de los datos personales",
+        adviseText2: "Las finalidades del tratamiento de sus datos personales serán las siguientes:",
+        advise1: "1.- Emitir comprobantes fiscales digitales",
+        advise2: "2.- Realizar depósitos bancarios en las cuentas del titular",
+        advise3: "3.- Desarrollar los proyectos solicitados por el titular con base a la información técnica del trabajo a realizar",
+
+        adviseTitle3: "Datos personales que se tratarán",
+        adviseText3: "Los datos personales que serán requeridos para cumplir con las finalidades señaladas en el párrafo anterior serán los siguientes:",
+        advise4: "1.- Nombre comercial del Titular, en caso de ser aplicable.",
+        advise5: "2.- Razón social del Titular en caso de ser aplicable.",
+        advise6: "3.- Nombre del Representante Legal del Titular, en caso de ser aplicable.",
+        advise7: "4.- Domicilio fiscal del Titular.",
+        advise8: "5.- Domicilio de la oficina corporativa o local comercial del Titular.",
+        advise9: "6.- Número telefónico y correo electrónico del Titular.",
+        advise10: "7.- Los datos bancarios del Titular (Banco, Cuenta, Cuenta Clabe, Nombre).",
+        adviseText4: "Adicionalmente, se protegerán los datos personales contenidos en la documentación que Usted como Titular, entrega a la Sociedad, mismos que se enlistan a continuación.",
+        advise11: "1.- Credencial para votar del Representante Legal o del mismo Titular.",
+        advise12: "2.- R1 del Titular expedido por la Secretaría de Hacienda y Crédito Público.",
+        advise13: "3.- Cualquier información técnica del trabajo a realizar, incluyendo planos, fotos y elementos en tercera dimensión.",
+
+        adviseTitle4: "Transferenciad de datos",
+        adviseText5: "La Sociedad NO llevará a cabo transferencias de los datos personales recabados y proporcionados por el Titular.",
+
+        adviseTitle5: "Medios para el ejercicio de los derechos arco y medios para limitar el uso o divulgación de sus datos personales",
+        adviseText6: "Usted como Titular, tiene derecho a conocer qué datos personales se tienen de usted, para qué se utilizan y las condiciones de uso que se les da (Acceso). Asimismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea inexacta o incompleta (Rectificación); que será eliminada de los registros o bases de datos cuando considere que la misma no está siendo utilizada conforme a los principios deberes y obligaciones previstas en la normativa (Cancelación); así como oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como Derechos ARCO.",
+        adviseText7: "Cualquier solicitud de limitación en el uso o divulgación de sus datos personales o cualquier acto relacionado con el tratamiento de sus datos personales, así como el ejercicio de los Derechos ARCO, usted como Titular se podrá comunicar directamente al número de teléfono 55 2605 3481, con la RESPONSABLE la Lic. Ana Luisa G. de Mutz, quién le hará saber los requisitos de su solicitud, información que deberá acompañar a ésta, plazos de respuesta y los medios de respuesta.",
+
+        adviseTitle6: "Revocación del consentimiento.",
+        adviseText8: "Cualquier solicitud de revocación en el uso de sus datos personales, usted como Titular se podrá comunicar directamente al número de teléfono 55 2605 3481, con la RESPONSABLE la Lic. Ana Luisa G. de Mutz, quién le hará saber los requisitos de su solicitud, información que deberá acompañar a ésta, plazos de respuesta y los medios de respuesta.",
+
+        adviseTitle7: "Cambios al aviso de privacidad.",
+        adviseText9: "El procedimiento para el cambio de este aviso de privacidad consiste en la obtención de la autorización correspondiente por parte del RESPONSABLE, el medio por el que se comunicará el contenido de dicho cambio, en su caso, será mediante la publicación en el dominio de internet: www.stevin.mx",
+        adviseText10: "Firmo de conformidad el presente aviso de privacidad y manifestó mi consentimiento para la utilización de mis datos personales:",
+        advise14: "Nombre:",
+        advise15: "Firma:",
+        advise16: "Fecha:"
     },
     en: {
         // Page Title
@@ -405,6 +448,7 @@ const translations = {
         machinesSubtitle: "We have a modern and well-equipped workshop for efficient results and to produce with the highest quality standards.",
         
         // Machine Specifications
+        technologieTitle: "Technology",
         machineTitle: "CNC Machines",
         machineTitle2: "Injection Molding Machines",
         machineTitle3: "Quality Assurance",
@@ -448,10 +492,52 @@ const translations = {
         footerMissionVision: "Mission and Vision",
         footerMachines: "Our Machines",
         footerContact: "Contact",
+        footerPrivacy: "Privacy Policy",
         footerCopyright: "© 2025 STEVIN. All rights reserved.",
         footerMadeBy: "Made by",
         privacyPolicy: "Privacy Policy",
-        termsOfUse: "Terms of Use"
+        termsOfUse: "Terms of Use",
+
+        // Privacy policy
+    adviseTitle: "Identity and address of the responsible party",
+    adviseText: "Based on Articles 15 and 16 of the Federal Law on the Protection of Personal Data Held by Private Parties, STEVIN, Sociedad Anónima de Capital Variable (hereinafter also referred to as the “Company”), through Lic. Ana Luisa G. de Mutz, located at Calle 3RA Sur No. 30, Colonia La Independencia, 54914, Tultitlán, State of Mexico, IS RESPONSIBLE for the use and protection of the personal data that you, as the Data Subject, will provide to the Company.",
+
+    adviseTitle2: "Purposes of processing personal data",
+    adviseText2: "The purposes of processing your personal data will be the following:",
+    advise1: "1.- Issue digital tax receipts",
+    advise2: "2.- Make bank deposits into the account of the data subject",
+    advise3: "3.- Develop the projects requested by the data subject based on the technical information of the work to be carried out",
+
+    adviseTitle3: "Personal data to be processed",
+    adviseText3: "The personal data required to fulfill the purposes mentioned in the previous paragraph will be the following:",
+    advise4: "1.- Trade name of the Data Subject, if applicable.",
+    advise5: "2.- Corporate name of the Data Subject, if applicable.",
+    advise6: "3.- Name of the Legal Representative of the Data Subject, if applicable.",
+    advise7: "4.- Tax address of the Data Subject.",
+    advise8: "5.- Address of the corporate office or business premises of the Data Subject.",
+    advise9: "6.- Telephone number and email address of the Data Subject.",
+    advise10: "7.- Banking information of the Data Subject (Bank, Account, CLABE Account, Name).",
+    adviseText4: "Additionally, the personal data contained in the documentation that you, as the Data Subject, deliver to the Company will be protected, which are listed below.",
+    advise11: "1.- Voting ID card of the Legal Representative or of the Data Subject.",
+    advise12: "2.- R1 of the Data Subject issued by the Ministry of Finance and Public Credit.",
+    advise13: "3.- Any technical information about the work to be carried out, including plans, photos, and 3D elements.",
+
+    adviseTitle4: "Data transfers",
+    adviseText5: "The Company will NOT carry out transfers of personal data collected and provided by the Data Subject.",
+
+    adviseTitle5: "Means for exercising ARCO rights and for limiting the use or disclosure of your personal data",
+    adviseText6: "You, as the Data Subject, have the right to know what personal data we hold about you, what it is used for, and the conditions of use given to it (Access). Likewise, it is your right to request the correction of your personal information if it is outdated, inaccurate, or incomplete (Rectification); that it be deleted from the records or databases when you consider that it is not being used in accordance with the principles, duties, and obligations set forth in the regulations (Cancellation); as well as to oppose the use of your personal data for specific purposes (Opposition). These rights are known as ARCO Rights.",
+    adviseText7: "Any request to limit the use or disclosure of your personal data or any act related to the processing of your personal data, as well as the exercise of ARCO Rights, may be made directly by contacting the phone number 55 2605 3481, with the RESPONSIBLE party, Lic. Ana Luisa G. de Mutz, who will inform you of the requirements for your request, the information that must accompany it, response deadlines, and means of response.",
+
+    adviseTitle6: "Revocation of consent",
+    adviseText8: "Any request for revocation of the use of your personal data may be made directly by contacting the phone number 55 2605 3481, with the RESPONSIBLE party, Lic. Ana Luisa G. de Mutz, who will inform you of the requirements for your request, the information that must accompany it, response deadlines, and means of response.",
+
+    adviseTitle7: "Changes to the privacy notice",
+    adviseText9: "The procedure for changing this privacy notice consists of obtaining the corresponding authorization from the RESPONSIBLE party. The means by which the content of such change will be communicated, if applicable, will be through publication on the website: www.stevin.mx",
+    adviseText10: "I sign in agreement with this privacy notice and declare my consent for the use of my personal data:",
+    advise14: "Name:",
+    advise15: "Signature:",
+    advise16: "Date:"
     },
     de: {
         // Page Title
@@ -515,6 +601,7 @@ const translations = {
         machinesSubtitle: "Wir verfügen über eine moderne und gut ausgestattete Werkstatt, um effiziente Ergebnisse auf höchsten Qualitätsstufen zu bieten.",
         
         // Machine Specifications
+        technologieTitle: "Technologie",
         machineTitle: "CNC-Maschine",
         machineTitle2: "Spritzgießmaschinen",
         machineTitle3: "Qualitätssicherung",
@@ -558,10 +645,53 @@ const translations = {
         footerMissionVision: "Mission und Vision",
         footerMachines: "Unsere Maschinen",
         footerContact: "Kontakt",
+        footerPrivacy: "DATENSCHUTZERKLÄRUNG",
         footerCopyright: "© 2025 STEVIN. Alle Rechte vorbehalten.",
         footerMadeBy: "Erstellt von",
         privacyPolicy: "Datenschutzrichtlinie",
-        termsOfUse: "Nutzungsbedingungen"
+        termsOfUse: "Nutzungsbedingungen",
+
+        // Aviso de privacidad
+
+        adviseTitle: "Identität und Adresse des Verantwortlichen",
+        adviseText: "Basierend auf den Artikeln 15 und 16 des Bundesgesetzes zum Schutz personenbezogener Daten im Besitz von Privatpersonen ist STEVIN, Sociedad Anónima de Capital Variable (nachfolgend auch als das „Unternehmen“ bezeichnet), vertreten durch Lic. Ana Luisa G. de Mutz, mit Sitz in Calle 3RA Sur Nr. 30, Colonia La Independencia, 54914, Tultitlán, Bundesstaat Mexiko, VERANTWORTLICH für die Nutzung und den Schutz der personenbezogenen Daten, die Sie als Betroffener dem Unternehmen zur Verfügung stellen.",
+
+        adviseTitle2: "Zwecke der Verarbeitung personenbezogener Daten",
+        adviseText2: "Die Zwecke der Verarbeitung Ihrer personenbezogenen Daten sind die folgenden:",
+        advise1: "1.- Ausstellung digitaler Steuerbelege",
+        advise2: "2.- Durchführung von Bankeinzahlungen auf das Konto des Betroffenen",
+        advise3: "3.- Entwicklung der vom Betroffenen angeforderten Projekte auf Grundlage der technischen Informationen der auszuführenden Arbeit",
+
+        adviseTitle3: "Zu verarbeitende personenbezogene Daten",
+        adviseText3: "Die personenbezogenen Daten, die erforderlich sind, um die im vorherigen Absatz genannten Zwecke zu erfüllen, sind die folgenden:",
+        advise4: "1.- Handelsname des Betroffenen, falls zutreffend.",
+        advise5: "2.- Firmenname des Betroffenen, falls zutreffend.",
+        advise6: "3.- Name des gesetzlichen Vertreters des Betroffenen, falls zutreffend.",
+        advise7: "4.- Steuerliche Anschrift des Betroffenen.",
+        advise8: "5.- Anschrift des Unternehmenssitzes oder Geschäftslokals des Betroffenen.",
+        advise9: "6.- Telefonnummer und E-Mail-Adresse des Betroffenen.",
+        advise10: "7.- Bankdaten des Betroffenen (Bank, Konto, CLABE-Konto, Name).",
+        adviseText4: "Darüber hinaus werden die personenbezogenen Daten geschützt, die in den Unterlagen enthalten sind, die Sie als Betroffener dem Unternehmen übergeben, und die nachstehend aufgeführt sind.",
+        advise11: "1.- Wählerausweis des gesetzlichen Vertreters oder des Betroffenen selbst.",
+        advise12: "2.- R1 des Betroffenen, ausgestellt vom Finanzministerium und der öffentlichen Kreditbehörde.",
+        advise13: "3.- Alle technischen Informationen über die auszuführende Arbeit, einschließlich Pläne, Fotos und 3D-Elemente.",
+
+        adviseTitle4: "Datenübermittlungen",
+        adviseText5: "Das Unternehmen wird KEINE Übermittlungen der vom Betroffenen erhobenen und bereitgestellten personenbezogenen Daten durchführen.",
+
+        adviseTitle5: "Möglichkeiten zur Ausübung der ARCO-Rechte und zur Einschränkung der Nutzung oder Weitergabe Ihrer personenbezogenen Daten",
+        adviseText6: "Sie als Betroffener haben das Recht zu erfahren, welche personenbezogenen Daten wir über Sie besitzen, wofür sie verwendet werden und unter welchen Bedingungen (Zugang). Ebenso haben Sie das Recht, die Berichtigung Ihrer personenbezogenen Daten zu verlangen, wenn diese veraltet, unrichtig oder unvollständig sind (Berichtigung); deren Löschung aus den Registern oder Datenbanken, wenn Sie der Ansicht sind, dass sie nicht gemäß den in den Vorschriften festgelegten Grundsätzen, Pflichten und Verpflichtungen verwendet werden (Löschung); sowie der Nutzung Ihrer personenbezogenen Daten für bestimmte Zwecke zu widersprechen (Widerspruch). Diese Rechte sind als ARCO-Rechte bekannt.",
+        adviseText7: "Jede Anfrage zur Einschränkung der Nutzung oder Weitergabe Ihrer personenbezogenen Daten oder jeder Akt im Zusammenhang mit der Verarbeitung Ihrer personenbezogenen Daten sowie die Ausübung der ARCO-Rechte können direkt telefonisch unter der Nummer 55 2605 3481 an die VERANTWORTLICHE, Lic. Ana Luisa G. de Mutz, gerichtet werden, die Sie über die Anforderungen Ihrer Anfrage, die beizufügenden Informationen, die Fristen für die Beantwortung und die Antwortmöglichkeiten informiert.",
+
+        adviseTitle6: "Widerruf der Einwilligung",
+        adviseText8: "Jede Anfrage zum Widerruf der Nutzung Ihrer personenbezogenen Daten kann direkt telefonisch unter der Nummer 55 2605 3481 an die VERANTWORTLICHE, Lic. Ana Luisa G. de Mutz, gerichtet werden, die Sie über die Anforderungen Ihrer Anfrage, die beizufügenden Informationen, die Fristen für die Beantwortung und die Antwortmöglichkeiten informiert.",
+
+        adviseTitle7: "Änderungen an der Datenschutzerklärung",
+        adviseText9: "Das Verfahren zur Änderung dieser Datenschutzerklärung besteht darin, die entsprechende Genehmigung der VERANTWORTLICHEN einzuholen. Die Art und Weise, wie der Inhalt einer solchen Änderung mitgeteilt wird, erfolgt gegebenenfalls durch Veröffentlichung auf der Website: www.stevin.mx",
+        adviseText10: "Ich unterzeichne diese Datenschutzerklärung in Übereinstimmung und erkläre meine Einwilligung zur Verwendung meiner personenbezogenen Daten:",
+        advise14: "Name:",
+        advise15: "Unterschrift:",
+        advise16: "Datum:"
     }
 };
 
